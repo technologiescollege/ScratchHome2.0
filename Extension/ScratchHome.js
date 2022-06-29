670cc28b390fabda55d2c1711b7674f2f35d8d22
kimokipo <hammi.kamal.mpsi@hotmail.com>
2022-06-29 09:40:32 +0200

    add creation of blocks for every object (Menu or simple block), and modification of position of observer sprite

--- Extension/ScratchHome.js
+++ Extension/ScratchHome.js
@@ -1,71 +1,194 @@
 class ScratchHome {
     static message = "empty";
     static status = "empty";
-    constructor() {
+    static x = 0;
+    static y = 0;
+    static agle = 0;
+    static sprite = undefined;
+
+    constructor(runtime) {
+        this.runtime = runtime;
         this.socket = null;
         this.wsUrl = "ws://localhost:8000";
+        ScratchHome.sprite = this.runtime.getSpriteTargetByName("Observer");
+        if (ScratchHome.sprite){
+            this.objectlist = ScratchHome.sprite.lookupVariableById("CNn7j*SP0QT%rN4=j[xz");
+            this.lamplist = ScratchHome.sprite.lookupVariableById("GoN|030ruZ,{H+4$)C-$");
+        }
     }
-    
-    getblocks(){
-        return [ 
-            {
-                "opcode": "setColor",
-                "blockType": "command",
-                "text": "colorer [object] en [colorList] ",
-                "arguments": {
-                    "object": {
-                        "type": "string",
-                        "defaultValue": "Lit bébé(827158385)"
-                    },
-                    "colorList": {
-                        "type": "string",
-                        "menu": "colorMenu"
-                    }                                
-                }
-            },
-            {
-                "opcode": "switchOnOff",
-                "blockType": "command",
-                "text": "[switchList] le/la [lamp]",
-                "arguments": {
-                    "switchList": {
-                        "type": "string",
-                        "menu": "switchMenu"
-                    },
-                    "lamp": {
-                        "type": "string",
-                        "defaultValue":  "Suspension(296454286)"
-                    },
-                }
-            },
-            {
-                "opcode": 'getMessage',
-                "blockType": "reporter",
-                "text": "message"
-            },
-            {
-                "opcode": 'getStatus',
-                "blockType": "reporter",
-                "text": "status"
-            }
-        ];
-    }
-    
+
     getInfo() {
         return {
             "id": "scratchhome",
             "name": "ScratchHome",
             "blocks": this.getblocks(),
-            "menus":{
-                "colorMenu":{
-                    "items": ["noir", "bleu", "cyan", "gris", "vert", "magenta", "rouge", "blanc", "jaune"]
-                },
-                "switchMenu":{
-                    "items": ["Allumer", "Eteindre"]
-                }
-            }
+            "menus":this.getMenus()
         };
     }
+
+    getBlocks(){
+        var typeBlocks = "";
+        var result =  [ {
+          "opcode": "setColor",
+          "blockType": "command",
+          "text": "colorer [object] en [colorList] ",
+          "arguments": {
+              "object": {
+                  "type": "string",
+                  "defaultValue": ""
+              },
+              "colorList": {
+                  "type": "string",
+                  "menu": "colorMenu"
+              }                                
+          }
+      },
+      {
+          "opcode": "switchOnOff",
+          "blockType": "command",
+          "text": "[switchList] le/la [lamp]",
+          "arguments": {
+              "switchList": {
+                  "type": "string",
+                  "menu": "switchMenu"
+              },
+              "lamp": {
+                  "type": "string",
+                  "defaultValue": ""
+              },
+          }
+      }];
+        if (ScratchHome.sprite){
+          typeBlocks = ScratchHome.sprite.lookupVariableById("L^i{fNhE#uQ8.g=D;O~O");
+          if (typeBlocks && typeBlocks.value.startsWith("m")){
+            result = [];
+            if (this.objectlist.value.length > 0){
+            result.push({
+              "opcode": "setColor",
+              "blockType": "command",
+              "text": "colorer [object] en [colorList] ",
+              "arguments": {
+                  "object": {
+                      "type": "string",
+                      "menu": "objectMenu"
+                  },
+                  "colorList": {
+                      "type": "string",
+                      "menu": "colorMenu"
+                  }                                
+              }
+          });
+            }
+            if (this.lamplist.value.length > 0){  
+          result.push({
+              "opcode": "switchOnOff",
+              "blockType": "command",
+              "text": "[switchList] le/la [lamp]",
+              "arguments": {
+                  "switchList": {
+                      "type": "string",
+                      "menu": "switchMenu"
+                  },
+                  "lamp": {
+                      "type": "string",
+                      "menu":  "lampMenu"
+                  }
+              }
+          });
+        } 
+        }
+      }  
+      
+          if (typeBlocks && typeBlocks.value.startsWith("b")){
+            result = []; 
+            for (let o of this.objectlist.value){
+              result.push({
+                "opcode": "setColor",
+                "blockType": "command",
+                "text": "colorer [object] en [colorList]",
+                "arguments": {
+                  "object": {
+                  "type": "string",
+                  "defaultValue": o
+                  },
+                    "colorList": {
+                        "type": "string",
+                        "menu": "colorMenu"
+                    }                                
+                }
+              });
+            }
+            for (let l of this.lamplist.value){
+              result.push({
+                "opcode": "switchOnOff",
+                "blockType": "command",
+                "text": "[switchList] le/la [lamp]",
+                "arguments": {
+                    "switchList": { 
+                        "type": "string",
+                        "menu": "switchMenu"
+                    },
+                    "lamp": {
+                        "type": "string",
+                        "defaultValue": l
+                    }
+                }
+            });
+          }
+        } 
+      result.push(
+        {
+            "opcode": 'getMessage',
+            "blockType": "reporter",
+            "text": "message"
+        });
+      result.push(
+        {
+            "opcode": 'getStatus',
+            "blockType": "reporter",
+            "text": "status"
+          });
+        result.push(
+            {
+                "opcode": 'getX',
+                "blockType": "reporter",
+                "text": "x"
+            });
+          result.push(
+            {
+                "opcode": 'getY',
+                "blockType": "reporter",
+                "text": "y"
+              });
+          result.push(
+                {
+                    "opcode": 'getAngle',
+                    "blockType": "reporter",
+                    "text": "angle"
+                  });
+      
+        return result;
+      }
+      getMenus(){
+        var objectMenu = [];
+        var lampMenu = [];
+        var result =  {
+          "colorMenu":{
+              "items": ["noir", "bleu", "cyan", "gris", "vert", "magenta", "rouge", "blanc", "jaune"]
+          },
+          "switchMenu":{
+              "items": ["Allumer", "Eteindre"]
+          }
+        };
+        if (this.objectlist && this.objectlist.value.length > 0){ 
+          result["objectMenu"] = {"items":this.objectlist.value}
+        } 
+        if (this.lamplist && this.lamplist.value.length > 0){
+          result["lampMenu"] = {"items":this.lamplist.value}
+        }
+        return result;
+      }
+    
     connect() {
         // open the connection if one does not exist
         if (this.socket !== null
@@ -83,7 +206,17 @@ class ScratchHome {
         };
   
         this.socket.onmessage = function(event) {
-            if (event.data){
+            var msgrec = event.data
+            if (msgrec.startsWith("position")){
+                var pos = msgrec.split("/")
+                ScratchHome.x = parseFloat(pos[1]);
+                ScratchHome.y = parseFloat(pos[2]);
+                ScratchHome.angle = parseFloat(pos[3]);
+                if (ScratchHome.sprite){
+                ScratchHome.sprite.setXY(ScratchHome.x,ScratchHome.y);
+                ScratchHome.sprite.setDirection(ScratchHome.angle);
+                } 
+            } else{
                 ScratchHome.message = event.data;
             } 
         };
@@ -125,10 +258,42 @@ class ScratchHome {
        * @return {string} - the current status.
        */
        getStatus () {
-		this.connect();
-		return ScratchHome.status;
-}
+            this.connect();
+            return ScratchHome.status;
+        }
+        /**
+         * Get the current message.
+         * @return {string} - the current x.
+         */
+        getX () {
+                return ScratchHome.x;
+        }
+  
+        /**
+         * Get the current status.
+         * @return {string} - the current y.
+         */
+         getY () {
+              return ScratchHome.y;
+          }
+          /**
+           * Get the current status.
+           * @return {string} - the current angle.
+           */
+           getAngle () {
+                return ScratchHome.angle;
+            }
 }
 
-Scratch.extensions.register(new ScratchHome())
+(function() {
+    var extensionClass = ScratchHome
+    if (typeof window === "undefined" || !window.vm) {
+        Scratch.extensions.register(new extensionClass())
+    }
+    else {
+        var extensionInstance = new extensionClass(window.vm.extensionManager.runtime)
+        var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
+        window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
+    }
+})()
 
